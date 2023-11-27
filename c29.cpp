8ffc53784b69b9814b2af55e2c5cc1cf724449a7
Rehan Ali maan <130174371+RehanAliMaan@users.noreply.github.com>
2023-11-27 21:40:16 +0500

    Add files via upload

---
+++ c29.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<conio.h>
+int main()
+{
+	char charac;
+	cout<<"enter your letter"<<endl;
+	cin>>charac;
+	cout<<"the two letters are"<<endl;
+	cout<<"(char)(charac+1"<<enl;
+	cout<<"(char)(charac+1"<<enl;
+	return 0;
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
